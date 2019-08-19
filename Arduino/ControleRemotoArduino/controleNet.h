977fd2b684b9b6a1d2d57a07e897464d7cfd1a22
Jairo <jairo.rotava@gmail.com>
2019-08-18 22:33:22 -0300

    Organizacao de arquivos

---
+++ Arduino/ControleRemotoArduino/controleNet.h
@@ -0,0 +1,22 @@
+#ifndef controleNet_h
+#define controleNet_h
+
+#define VOLUME_UP   E17AB04F  
+#define VOLUME_DOWN E17A708F
+#define CHANNEL_UP  E17A08F7
+#define CHANNEL_DOWN E17A58A7
+#define UP            E17AD02F
+#define DOWN       E17A30CF   
+#define LEFT      E17AD827
+#define RIGHT     E17A38C7
+#define OK     E17AA857 
+#define FWD   E17AAC53
+#define REW   E17A2CD3
+#define PLAY  E17A6C93
+#define STOP  E17A4CB
+
+#define REPEAT_KEY FFFFFFFF
+
+
+
+#endif 
---
+++ Arduino/ControleRemotoArduino/controlePanasonic.h
@@ -0,0 +1,39 @@
+#ifndef controlePanasonic_h
+#define controlePanasonic_h
+
+//Algumas teclas quando pressionadas/solta repetem o codigo por 3 vezes
+//Quando elas ficam apertadas não solta nada por um tempo e depois
+//soltam outro codigo.
+
+#define VOLUME_UP   BE3BB37  
+#define VOLUME_DOWN D4D9F2A7
+#define UP            C20370A1
+#define DOWN       81930A09   
+#define LEFT      983AB4C1
+#define RIGHT     21035431
+#define OK     BB0ED9E1 
+#define PLAY  DEC919A5
+#define STOP  1D15F4D7
+#define RETURN ABBE1086
+#define EXIT 6E445258
+#define POWER 406A954D  
+
+//Teclas especiais
+
+//Aperta/solta (repete 3x)
+#define NEXT   2E8C2A89 
+//Quando so apertado 
+#define FWD 59135E7
+
+//Aperta/solta (repete 3x)
+#define PREVIOUS   1F6E01C9
+//Quando so apertado 
+#define REW A23B903F 
+
+//Aperta/solta (repete 3x)
+#define PLAY_PAUSE AA7B2167
+//Quando so apertado
+#define PLAY_PAUSE_PRESSED 59135E7
+
+
+#endif //controlePanasonic_h
